1f1525b7a6193fca0815e32f7860e29092d90519
Ding_YF <DingYFA@yeah.net>
2021-11-05 14:45:23 +0800

    更新图片传输

--- scripts/Network_client.py
+++ scripts/Network_client.py
@@ -1,21 +1,25 @@
 from socket import *
+import cv2
+import struct
 
 HOST = '169.254.200.85'
 PORT = 8080
 BUFSIZ = 1024
 ADDR = (HOST,PORT)
 
+cap = cv2.VideoCapture(0)
+
 tcpCliSock = socket(AF_INET,SOCK_STREAM)
 tcpCliSock.connect(ADDR)
 
-while True:
-    data = input(">>>")
-    if not data:
-        break
-
-    tcpCliSock.send(bytes(data,'utf-8'))
-    data = tcpCliSock.recv(BUFSIZ)
-    if not data:
-        break
-    print(data.decode('utf-8'))
+while cap.isOpened():
+    (ret,frame) = cap.read()
+    #data = input(">>>")
+    img = frame
+    img_pack = struct.pack(b'128sq', bytes(img, encoding='utf-8')) #os.stat(filepath).st_size 读取文件大小
+    tcpCliSock.send(img_pack)
+    #data = tcpCliSock.recv(BUFSIZ)
+    #print(data.decode('utf-8'))
+    cv2.imshow('test', frame)
+    cv2.waitKey(15)
 tcpCliSock.close()
--- scripts/Network_server.py
+++ scripts/Network_server.py
@@ -1,5 +1,6 @@
 from socket import *
 from time import ctime
+import struct
 
 HOST = ''   #169.254.200.85
 PORT = 8080
@@ -10,17 +11,17 @@ tcpSvrSock = socket(AF_INET,SOCK_STREAM)
 tcpSvrSock.bind(ADDR)
 tcpSvrSock.listen(5)
 
-class Server():
-        print("waiting for connection !!!")
-        tcpCliSock,addr = tcpSvrSock.accept()
-        print("connect form",addr)
-        while True:
-            data = tcpCliSock.recv(BUFSIZ)
-            if not data:
-                break
-            data = data.decode('utf-8')
-            #print(data)
-            break
-            # respMsg = "[%s] %s" % (ctime(),data)
-            # tcpCliSock.send(bytes(respMsg,'utf-8'))
-        tcpCliSock.close()
+while True:
+    print("waiting for connection !!!")
+    tcpCliSock,addr = tcpSvrSock.accept()
+    print("connect form",addr)
+
+    while True:
+        #data = tcpCliSock.recv(BUFSIZ)
+        fileinfo_size = struct.calcsize('128sq')
+        img_recv = tcpCliSock.recv(fileinfo_size)
+        if img_recv:
+            filename, filesize = struct.unpack('128sq', img_recv)
+            fn = filename.decode().strip('\x00')  #去掉不可见的字符 \x00
+    tcpCliSock.close()
+tcpCliSock.close()
