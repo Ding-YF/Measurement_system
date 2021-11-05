5fcf2e36405dd94f9836681fc14d21a010a0f983
Ding_YF <DingYFA@yeah.net>
2021-11-05 11:17:34 +0800

    将服务端更改成可调用的类

--- scripts/Network_server.py
+++ scripts/Network_server.py
@@ -10,20 +10,17 @@ tcpSvrSock = socket(AF_INET,SOCK_STREAM)
 tcpSvrSock.bind(ADDR)
 tcpSvrSock.listen(5)
 
-while True:
-    print("waiting for connection !!!")
-    tcpCliSock,addr = tcpSvrSock.accept()
-    print("connect form",addr)
-
-    while True:
-        data = tcpCliSock.recv(BUFSIZ)
-
-        if not data:
+class Server():
+        print("waiting for connection !!!")
+        tcpCliSock,addr = tcpSvrSock.accept()
+        print("connect form",addr)
+        while True:
+            data = tcpCliSock.recv(BUFSIZ)
+            if not data:
+                break
+            data = data.decode('utf-8')
+            #print(data)
             break
-        data = data.decode('utf-8')
-        #print(data)
-        respMsg = "[%s] %s" % (ctime(),data)
-        tcpCliSock.send(bytes(respMsg,'utf-8'))
-
-    tcpCliSock.close()
-tcpCliSock.close()
+            # respMsg = "[%s] %s" % (ctime(),data)
+            # tcpCliSock.send(bytes(respMsg,'utf-8'))
+        tcpCliSock.close()
