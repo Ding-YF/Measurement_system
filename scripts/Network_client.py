cd8512f82ae0477db97792dc6212ab11a3d11e56
Ding_YF <DingYFA@yeah.net>
2021-11-05 18:27:45 +0800

    客户端可持续发送信息

--- scripts/Network_client.py
+++ scripts/Network_client.py
@@ -12,10 +12,5 @@ while True:
     data = input(">>>")
     if not data:
         break
-
     tcpCliSock.send(bytes(data,'utf-8'))
-    data = tcpCliSock.recv(BUFSIZ)
-    if not data:
-        break
-    print(data.decode('utf-8'))
 tcpCliSock.close()
--- scripts/Network_server.py
+++ scripts/Network_server.py
@@ -14,16 +14,11 @@ while True:
     print("waiting for connection !!!")
     tcpCliSock,addr = tcpSvrSock.accept()
     print("connect form",addr)
-
     while True:
         data = tcpCliSock.recv(BUFSIZ)
-
         if not data:
             break
         data = data.decode('utf-8')
-        #print(data)
-        respMsg = "[%s] %s" % (ctime(),data)
-        tcpCliSock.send(bytes(respMsg,'utf-8'))
-
+        print(data)
     tcpCliSock.close()
 tcpCliSock.close()
