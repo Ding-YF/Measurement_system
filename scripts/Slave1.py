c9190bd1d1b83152ecc522e58318966957d1bb6f
Ding_YF <DingYFA@yeah.net>
2021-11-07 10:34:59 +0800

    增加通讯|根据接受的指令运行|增加等待部分

--- scripts/Slave1.py
+++ scripts/Slave1.py
@@ -11,18 +11,26 @@ from numpy import *
 from scipy import optimize
 import functools
 import threading
+from socket import *
 from matplotlib import pyplot as p
 
 import double_multi as dm
 
-cap = cv2.VideoCapture(0)
+#输出log
+# import sys
+# f_handler=open('out.log', 'w')
+# sys.stdout=f_handler
+
+cap = cv2.VideoCapture(1)
 # fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+
 
 red = [(156,43,46),(180,255,255)]
 green = [(35,43,46),(77,255,255)]
 
 frame_img = None
+tcpCliSock = None
 x_points = []
 y_points = []
 x_pointss = [36, 36, 19, 18, 33, 26,18,20,-22.564]
@@ -30,9 +38,15 @@ y_pointss = [14, 10, 28, 31, 18, 26,30,32,10.457]
 c = 1
 timeF = 8
 
+HOST = '169.254.37.134'
+PORT = 8080
+ADDR = (HOST,PORT)
+BUFSIZE = 1024
+
 def get_img():
     global frame_img
     while cap.isOpened():
+        # print('--1111--')
         (ret,frame) = cap.read()
         frame = cv2.flip (frame,1)
         frame_img = frame
@@ -54,7 +68,7 @@ def find_point(img):
             contour_area = math.fabs(cv2.contourArea(cn))
             # if (contour_area>160 and contour_area < 350):
             if (contour_area > 100 and contour_area < 400):
-                print('-77-')
+                # print('-77-')
                 # print(cn)   ####程序到这里报错，因为cn是个轮廓
                 x_point, y_point, w, h = cv2.boundingRect(cn) #contours[cn]
                 # if (contour_area > 100 and contour_area < 400):
@@ -63,22 +77,44 @@ def find_point(img):
                 y_points.append(y_point)
                 x_m = mean(x_points)
                 y_m = mean(y_points)
+def client(ADDR):
+    global tcpCliSock
+    print('--444--')
+    tcpCliSock = socket(AF_INET, SOCK_STREAM)
+    print('--555--')
+    tcpCliSock.connect(ADDR)
+    print('connect to',ADDR)
+th2=threading.Thread(target=client,name="设备一",args=(ADDR,))
+th2.setDaemon(True)
+th2.start()
 
 if __name__ == '__main__':
     key = 0
     while True:
         if frame_img is None:
             print('init')
-        elif (key == 0):
+            time.sleep(0.3)
+        elif (key == 1):
+            print('--in 1--')
+            # R = 30
+            # time.sleep(3)
+            # if tcpCliSock is not None:
+            #     print('--222--')
+            #     tcpCliSock.send(bytes(str(R), 'utf-8'))
+            # print(R)
+    ######################## for test ##########################
             find_point(frame_img)
-            if len(x_points) == 500:
+            if len(x_points) == 1000:
                 R = dm.ys(x_points,y_points)
+                if tcpCliSock is not None:
+                    tcpCliSock.send(bytes(str(R), 'utf-8'))
+                x_points = []
+                y_points = []
                 print(R)
                 key+=1
         else:
             print('wait')
+            key = tcpCliSock.recv(BUFSIZE)
+            key = int(key)
             time.sleep(0.5)
             continue
-                #fitting()
-            # cv2.imshow('frame_img', frame_img)
-            # cv2.waitKey(15)
