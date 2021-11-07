57f494f160c62ba9f79db1e2114eafe4119d3ced
Ding_YF <DingYFA@yeah.net>
2021-11-07 10:43:09 +0800

    可选择客户端定向发送信息

--- scripts/Network_server.py
+++ scripts/Network_server.py
@@ -7,9 +7,11 @@ PORT1 = 8080
 PORT2=8081
 BUFSIZ = 1024
 
-
+tcpSvrSock=0
+tcpSvrSock2=0
 
 def tcp1(PORT): #第一台设备端口为8080
+    global tcpCliSock,addr
     tcpSvrSock = socket(AF_INET, SOCK_STREAM)
     tcpSvrSock.bind((HOST,PORT))
     tcpSvrSock.listen(5)
@@ -17,35 +19,46 @@ def tcp1(PORT): #第一台设备端口为8080
         print("waiting first for connection !!!")
         tcpCliSock,addr = tcpSvrSock.accept()
         print("connect form",addr)
-        while True:
-            data = tcpCliSock.recv(BUFSIZ)
-            if not data:
-                break
-            data = data.decode('utf-8')
-            # print('data from ',addr,data )
-        tcpCliSock.close()
+
 
 def tcp2(PORT2): #第二台设备端口为8081
+    global tcpCliSock2,addr2
     tcpSvrSock2 = socket(AF_INET, SOCK_STREAM)
     tcpSvrSock2.bind((HOST,PORT2))
     tcpSvrSock2.listen(5)
     while True:
         print("waiting second for connection !!!")
-        tcpCliSock2,addr = tcpSvrSock2.accept()
+        tcpCliSock2,addr2 = tcpSvrSock2.accept()
         print("connect form",addr)
-        while True:
-            data2 = tcpCliSock2.recv(BUFSIZ)
-            if not data2:
-                break
-            data2 = data2.decode('utf-8')
-            # print('data2 from ',addr ,data2)
-        tcpCliSock2.close()
+
 
 #多线程
 t1=threading.Thread(target=tcp1,name="设备一",args=(PORT1,))
 t2=threading.Thread(target=tcp2,name="设备二",args=(PORT2,))
 t1.start()
 t2.start()
+
+
+
+while True:
+    op = input(">>>")
+    if(op=="1"):
+        tcpCliSock.send(bytes(op, 'utf-8'))
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        data = data.decode('utf-8')
+        print('data from ', addr, data)
+    elif(op=="2"):
+        tcpCliSock2.send(bytes(op, 'utf-8'))
+        data = tcpCliSock2.recv(BUFSIZ)
+        if not data:
+            break
+        data = data.decode('utf-8')
+        print('data from ', addr2, data)
+
+tcpCliSock.close()
+
 
 if __name__ == '__main__':
     pass
