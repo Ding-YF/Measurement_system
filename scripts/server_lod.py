0ee9529d43bddce27c457f6c79b2f31ca031a655
Ding_YF <DingYFA@yeah.net>
2021-11-07 23:05:51 +0800

    备份旧服务端

---
+++ scripts/server_lod.py
@@ -0,0 +1,24 @@
+from socket import *
+from time import ctime
+
+HOST = ''   #169.254.200.85
+PORT = 8080
+BUFSIZ = 1024
+ADDR = (HOST,PORT)
+
+tcpSvrSock = socket(AF_INET,SOCK_STREAM)
+tcpSvrSock.bind(ADDR)
+tcpSvrSock.listen(5)
+
+while True:
+    print("waiting for connection !!!")
+    tcpCliSock,addr = tcpSvrSock.accept()
+    print("connect form",addr)
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        data = data.decode('utf-8')
+        print(data)
+    tcpCliSock.close()
+tcpCliSock.close()
